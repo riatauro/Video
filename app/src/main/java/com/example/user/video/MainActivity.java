3b82541ae84fc87261d4661e14c8a6fe7b86f20d
riatauro <tauroelevita@gmail.com>
2016-02-23 23:04:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/video/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.user.video;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView =(VideoView)findViewById(R.id.videoView1);
+
+        //Creating MediaController
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+
+        //specify the location of media file
+      //  Uri uri=Uri.parse(Environment.getExternalStorageDirectory().getPath()+"/media/1.mp4");
+
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() +"/" + R.raw.dilwale));
+        videoView.requestFocus();
+        videoView.start();
+
+    }
+
+
+
+}
